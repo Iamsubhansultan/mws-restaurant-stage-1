edd30e48477a151c1c114a9b64ab36cb730ef834
IamAbdusSubhan <34831980+Iamsubhansultan@users.noreply.github.com>
2018-07-06 00:45:58 +0530

    serviceWorkerGoingIntoRootFolder

--- js/registering-sw.js
+++
@@ -1,8 +0,0 @@
-if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('js/sw.js')
-    .then(registration => {
-        console.log(`Registered!! || Scope:  ${registration.scope}`);
-	}).catch(error => {
-		console.log(`Failed: , error: ${error}`);
-	});
-}
