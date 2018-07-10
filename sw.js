1f1b44600fecce9667ec03889ff3283e3f76c289
IamAbdusSubhan <34831980+Iamsubhansultan@users.noreply.github.com>
2018-07-10 18:39:12 +0530

    update sw.js

--- sw.js
+++ sw.js
@@ -1,58 +1,63 @@
-var staticCacheName = 'restaurant-review-stage-v1';
-
-// TODO: install new cache
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-		return cache.addAll([
-		'/',
-		'index.html',
-		'restaurant.html',
-		'css/styles.css',
-		'css/mediacss.css',
-		'data/restaurants.json',
-		'js/dbhelper.js',
-		'js/main.js',
-		'js/registering-sw.js',
-		'js/restaurant_info.js',
-		'img/1.jpg',
-		'img/2.jpg',
-		'img/3.jpg',
-		'img/4.jpg',
-		'img/5.jpg',
-		'img/6.jpg',
-		'img/7.jpg',
-		'img/8.jpg',
-		'img/9.jpg',
-		'img/10.jpg'
-		]);
-	})
-   );
+var CACHE_VERSION = 'restaurant-reviews';
+var CACHE_FILES = [  
+	'/', 
+	'/index.html', 
+	'/restaurant.html', 
+	'/css/styles.css', 
+	'/js/dbhelper.js', 
+	'/js/main.js', 
+	'/data/restaurants.json', 
+	'/js/restaurant_info.js', 
+	'/img/1.jpg', 
+	'/img/2.jpg', 
+	'/img/3.jpg', 
+	'/img/4.jpg', 
+	'/img/5.jpg',
+	'/img/6.jpg',
+	'/img/7.jpg', 
+	'/img/8.jpg',
+	'/img/9.jpg', 
+	'/img/10.jpg'
+];
+self.addEventListener('install', function (event) { 
+	event.waitUntil( 
+		caches.open(CACHE_VERSION).then(function (cache) { 
+		return cache.addAll(CACHE_FILES);
+		})
+	);
 });
 
-// TODO: delete old caches
-self.addEventListener('activate',function(event) {
-	event.waitUntil(
-	    caches.keys().then(function(cacheNames) {
-			return Promise.all(
-			cacheNames.filter(function(cacheName) {
-				return cacheName.startsWith('restaurant-') && (staticCacheName != cacheName);
-            }).map(function(cacheName) {
-				return caches.delete(cacheName);
-        })
-      );
-    })
-  );
-});
-
-// TODO: fetch request
-self.addEventListener('fetch', function(event) {
-	var requestUrl = new URL(event.request.url);
-	event.respondWith(
-    caches.match(event.request).then(function(response) {
-		return response || fetch(event.request);
-	}).catch(function() {
-      return ('fetchis failed ...');
-    })
-  );
+self.addEventListener('fetch', function(event) { 
+	const url = new URL(event.request.url);
+	if (url.pathname.startsWith('/restaurant.html')) { 
+		event.respondWith(
+			caches.match('restaurant.html') .then(response => {
+				if(response !== undefined) { 
+					const cacheResponse = response;
+					caches.open(CACHE_VERSION).then(function (cache) { 
+						fetch(event.request).then(function(responseFetch) { 
+							cache.put('restaurant.html', responseFetch.clone()); 
+						})
+					});
+					return response; 
+				} 
+				return fetch(event.request);
+			})
+		);
+		return;
+	}
+	event.respondWith( 
+		caches.match(event.request).then(function(response) { 
+			if(response !== undefined) { 
+				const cacheResponse = response; 
+				caches.open(CACHE_VERSION).then(function (cache) { 
+					fetch(event.request).then(function(responseFetch) { 
+						cache.put(event.request, responseFetch.clone());
+					})
+				}); 
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
 });
